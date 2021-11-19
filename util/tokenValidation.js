d5becd60592d96115220c24b2fea31679689e0ae
DarshanMamtani <mamtanidarshan9@gmail.com>
2021-11-19 15:15:22 +0530

    Checking error in JWT validation

--- util/tokenValidation.js
+++ util/tokenValidation.js
@@ -32,7 +32,7 @@ const verifyToken = async (req, res, next) => {
             }
         }
         catch (error) {
-            return res.status(500).json({ message: "Error finding user" });
+            return res.status(500).json({ message: "Error finding user", error: error });
         }
     } else {
         // if no token given
